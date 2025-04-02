65d89088fef563e0c6fc549ca1a702ac561093b5
Dylan <dylantgreene92@gmail.com>
2025-04-02 15:53:35 -0500

    Remove tailwind config

--- content/tailwindcss/vite.config.mjs
+++ content/tailwindcss/vite.config.mjs
@@ -3,15 +3,13 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
-import tailwindConfig from './tailwind.config.js';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     open: true,
     port: 3000,
   },
-  plugins: [react(), tailwindcss(tailwindConfig)],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
